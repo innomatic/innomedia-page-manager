8baf228c69e5b6a2c0aad6ecb0294dd5c423af5a
Alex Pagnoni <alex.pagnoni@innoteam.it>
2014-04-08 11:58:19 +0200

    Added working dropzone file uploader

--- source/core/classes/shared/wui/WuiDropzone.php
+++ source/core/classes/shared/wui/WuiDropzone.php
@@ -18,8 +18,9 @@ class WuiDropzone extends \Innomatic\Wui\Widgets\WuiWidget
 
         $dropzoneJs = '';
 
+        $container = \Innomatic\Core\InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer');
+
         if (!isset($included)) {
-            $container = \Innomatic\Core\InnomaticContainer::instance('\Innomatic\Core\InnomaticContainer');
             $dropzoneJs = '<script src="'.$container->getBaseUrl(false).'/shared/dropzone.js"></script>
                 <link href="'.$container->getBaseUrl(false).'/shared/dropzone.css" type="text/css" rel="stylesheet">';
             $included = true;
@@ -37,19 +38,38 @@ console.log(myDropzone);
 <div id="dropzone" style="width: 100%; height: 200px;"><form action="http://www.torrentplease.com/dropzone.php" class="dropzone" id="demo-upload">
 </form></div>
          */
+
+        $id           = isset($this->mArgs['id']) ? $this->mArgs['id'] : $this->mName;
+        $pageModule   = $this->mArgs['pagemodule'];
+        $pageName     = $this->mArgs['pagename'];
+        $pageId       = $this->mArgs['pageid'];
+        $blockModule  = $this->mArgs['blockmodule'];
+        $blockName    = $this->mArgs['blockname'];
+        $blockCounter = $this->mArgs['blockcounter'];
+        $fileId       = $this->mArgs['fileid'];
+
+        $fileParameters = $pageModule.'/'.$pageName.'/'.$pageId.'/'.$blockModule.'/'.$blockName.'/'.$blockCounter.'/'.$fileId;
+
         $this->mLayout = $dropzoneJs.'
-<div id="'.$this->mName.'"></div>
+<div id="'.$id.'"></div>
 <script>
-alert(\'pino\');
-var myDropzone = new Dropzone("#'.$this->mName.'", { url: "/file/post"});
-document.querySelector("#'.$this->mName.'").classList.add("dropzone");
+var dropzone = new Dropzone("#'.$id.'", { url: "'.$container->getBaseUrl(false).'/dropzone/'.$fileParameters.'"';
+
+        if (isset($this->mArgs['maxfiles'])) {
+            $this->mLayout .= ', maxFiles: '.$this->mArgs['maxfiles'];
+        }
+
+        $this->mLayout .= '});';
+
+$this->mLayout .=
+'document.querySelector("#'.$id.'").classList.add("dropzone");
 console.log(myDropzone);
 </script>';
-        $event_data = new \Innomatic\Wui\Dispatch\WuiEventRawData($this->mArgs['disp'], $this->mName, 'file');
+
 /*
         $this->mLayout = ($this->mComments ? '<!-- begin ' . $this->mName . ' dropzone -->' : '') .
             $dropzoneJs.
-            '<form action="/file/upload" class="dropzone" id="'.$this->mName.'dropzone"></form>'.
+            '<form action="/file/upload" class="dropzone" id="'.$id.'dropzone"></form>'.
             ($this->mComments ? '<!-- end ' . $this->mName . " dropzone -->\n" : '');
  */
         return true;
