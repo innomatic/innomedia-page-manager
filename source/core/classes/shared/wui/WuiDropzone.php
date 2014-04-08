92f3ff836d6c7220eec356c3aa7731d513feb108
Alex Pagnoni <alex.pagnoni@innoteam.it>
2014-04-08 18:48:57 +0200

    Improved code

--- source/core/classes/shared/wui/WuiDropzone.php
+++ source/core/classes/shared/wui/WuiDropzone.php
@@ -25,19 +25,6 @@ class WuiDropzone extends \Innomatic\Wui\Widgets\WuiWidget
                 <link href="'.$container->getBaseUrl(false).'/shared/dropzone.css" type="text/css" rel="stylesheet">';
             $included = true;
         }
-/*
-        $objResponse->addIncludeScript($container->getBaseUrl(false).'/shared/dropzone.js');
-        $objResponse->addScript('
-            var myDropzone = new Dropzone("#dropzone", { url: "/file/post"});
-document.querySelector("#dropzone").classList.add("dropzone");
-console.log(myDropzone);
-        ');
-
-*/
-        /*
-<div id="dropzone" style="width: 100%; height: 200px;"><form action="http://www.torrentplease.com/dropzone.php" class="dropzone" id="demo-upload">
-</form></div>
-         */
 
         $id           = isset($this->mArgs['id']) ? $this->mArgs['id'] : $this->mName;
         $pageModule   = $this->mArgs['pagemodule'];
@@ -50,7 +37,8 @@ console.log(myDropzone);
 
         $fileParameters = $pageModule.'/'.$pageName.'/'.$pageId.'/'.$blockModule.'/'.$blockName.'/'.$blockCounter.'/'.$fileId;
 
-        $this->mLayout = $dropzoneJs.'
+        $this->mLayout = ($this->mComments ? '<!-- begin ' . $this->mName . ' dropzone -->' : '') .
+        $this->mLayout .= $dropzoneJs.'
 <div id="'.$id.'"></div>
 <script>
 var dropzone = new Dropzone("#'.$id.'", { url: "'.$container->getBaseUrl(false).'/dropzone/'.$fileParameters.'"';
@@ -63,15 +51,10 @@ var dropzone = new Dropzone("#'.$id.'", { url: "'.$container->getBaseUrl(false).
 
 $this->mLayout .=
 'document.querySelector("#'.$id.'").classList.add("dropzone");
-console.log(myDropzone);
 </script>';
 
-/*
-        $this->mLayout = ($this->mComments ? '<!-- begin ' . $this->mName . ' dropzone -->' : '') .
-            $dropzoneJs.
-            '<form action="/file/upload" class="dropzone" id="'.$id.'dropzone"></form>'.
-            ($this->mComments ? '<!-- end ' . $this->mName . " dropzone -->\n" : '');
- */
+        $this->mLayout .= $this->mComments ? '<!-- end ' . $this->mName . " dropzone -->\n" : '';
+
         return true;
     }
 }
